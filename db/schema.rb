acae3c911f082fb6ea4aeefc6713a5b59a8f2500
artemaminov <artemaminov@gmail.com>
2023-08-20 23:13:36 +0500

    create user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_18_113833) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_20_181238) do
   create_table "chats", force: :cascade do |t|
     t.integer "chat_id"
     t.string "title"
@@ -36,6 +36,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_18_113833) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["message_id"], name: "index_products_on_message_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "messages", "chats"
