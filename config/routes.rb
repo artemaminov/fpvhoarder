cdc5cbd1ec9df3fb1f0a7564b879b27fb8f13699
artemaminov <artemaminov@gmail.com>
2023-12-05 18:23:23 +0300

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
   root "home#index"
+  post "/signin", to: "sessions#create"
+  post "/logout", to: "sessions#destroy"
   post "/command/:command", to: "tg/command#send_to_tg"
-  resources :messages
-  post "/signin", to: "users#create"
+
+  resource :user, path: :profile do
+    resources :messages
+  end
 end
